5b08f78e070ac3a77fa56a4052944913a87a9072
juliazschwartz <90219896+juliazschwartz@users.noreply.github.com>
2023-02-03 10:53:02 -0300

    finalização da paginação

--- app/Views/componentes/pagination.php
+++ app/Views/componentes/pagination.php
@@ -1,14 +1,18 @@
-<?php $active = $_GET['page'] ?? '' ;
+<?php $active = $_GET['page'] ?? 1 ;
 $order = $_GET['order'] ?? '';
 $par = $_GET['par'] ?? '';
 ?>
 <nav aria-label="Page navigation" style="align-self: center;
 margin: 10px auto;">
-                      <ul class="pagination">
+  <ul class="pagination">
+<?php
+if($active > 1){
+?>
                         <li class="page-item prev">
-                              <a class="page-link" href="javascript:void(0);"><i class="tf-icon bx bx-chevrons-left"></i></a>
+                              <a class="page-link" href="?page=<?=$active -1?>"><i class="tf-icon bx bx-chevrons-left"></i></a>
                             </li>
                       <?php
+}
                       if($paginacao['pages'] <=5 ){
                         for($page = 1; $page <= $paginacao['pages']; $page++){
                         
@@ -25,16 +29,19 @@ margin: 10px auto;">
                       }
 
                       else{
-                        for($page = 1; $page <= 5; $page++){
-                        
+                        for($page = 1; $page <= $paginacao['pages']; $page++){
+                          for($pag = $active; $pag <($active + 5); $pag++){
+                          if($pag>=($paginacao['pages'])){
+                            break;
+                          }
                             ?>
-                                                <li class="page-item prev">
-                                                <li class="page-item" id="pag<?=$page?>">
-                                                  <a class="page-link" href="?page=<?=$page?>&order=<?=$order?>&par=<?=$par?>"><?=$page?></a>
-                                                </li>
-                                                <?php 
-                                           
-                                            }
+                            <li class="page-item prev">
+                                            <li class="page-item" id="pag<?=$pag?>">
+                                              <a class="page-link" href="?page=<?=$pag?>&order=<?=$order?>&par=<?=$par?>"><?=$pag?></a>
+                                            </li>
+                        <?php  }
+                          break;
+                         }
                       
                       
                      ?>
@@ -44,10 +51,16 @@ margin: 10px auto;">
                                                 <li class="page-item" id="pag<?=$paginacao['pages']?>">
                                                                              <a class="page-link" href="?page=<?=$paginacao['pages']?>&order=<?=$order?>&par=<?=$par?>"><?=$paginacao['pages']?> </a>
                                                                            </li>
-                                                <?php } ?>      
-                                                                           <li class="page-item prev">
-                                                         <a class="page-link" href="javascript:void(0);"><i class="tf-icon bx bx-chevrons-right"></i></a>
+                                                <?php }
+                                                if($active < $paginacao['pages']){
+                                                  ?>
+                                                                             <li class="page-item prev">
+                                                         <a class="page-link" href="?page=<?=$active +1?>"><i class="tf-icon bx bx-chevrons-right"></i></a>
                                                        </li>
+                                                                        <?php
+                                                  }
+                                                ?>      
+                                                                       
                           </ul>
                         </nav>
 
